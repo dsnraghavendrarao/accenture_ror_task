1ab9449e7376c2c5d749ccb0dfe2ece7d1128783
dsnraghavendrarao <raghavann13@gmail.com>
2019-07-07 09:25:27 +0530

    Sorry for updating late while cross checking i missed update file to commit

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,7 +3,8 @@ class Role < ApplicationRecord
 	# has_one :reporter, class_name: "Role", foreign_key: :report_role_id
 	validates_uniqueness_of :name
 	before_destroy do
-		self.employees.update_all report_role_id: (self.prior + 1)
+		reporte_role_id = Role.find_by_id(self.prior - 1)
+		self.employees.update_all report_role_id: (report_role_id.try(:id))
 		Role.where(prior: self.prior..Role.count).map{|role| role.update prior: (role.prior - 1)}
 	end
 	def response
